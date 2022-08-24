1437f7598aeb7f3d43e587315a86f0eba5fb7b56
ruramgwaluc <rurangwajl7@gmail.com>
2022-08-24 10:42:56 +0200

    update commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-const navbar = document.querySelector('#navbar');
+let navbar = document.querySelector('.header .navbar');
 
-const toggle = () => {
-    if(navbar.style.width == "220px"){
-        navbar.style.width = '0';
-    }
-    else{
-        navbar.style.width = '220px';
-    }
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
 }
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
+
+const wrapper = document.querySelector('.wrapper');
+const indicators = [...document.querySelectorAll('.indicators button')];
+
+let currentTestimonial = 0; // Default 0
+
+indicators.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        indicators[currentTestimonial].classList.remove('active');
+        wrapper.style.marginLeft = `-${100 * i}%`;
+        item.classList.add('active');
+        currentTestimonial = i;
+    })
+})
